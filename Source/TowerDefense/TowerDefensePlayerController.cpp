15afaa00ff3abff43d2e428d12b1f8400917896c
Shree Chowkwale <shree.chowkwale@gmail.com>
2014-05-23 15:08:22 -0700

    Adding autogenerated TowerDefensePlayerController

---
+++ Source/TowerDefense/TowerDefensePlayerController.cpp
@@ -0,0 +1,13 @@
+
+
+#include "TowerDefense.h"
+#include "TowerDefensePlayerController.h"
+
+
+ATowerDefensePlayerController::ATowerDefensePlayerController(const class FPostConstructInitializeProperties& PCIP)
+	: Super(PCIP)
+{
+
+}
+
+
---
+++ Source/TowerDefense/TowerDefensePlayerController.h
@@ -0,0 +1,18 @@
+
+
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+#include "TowerDefensePlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ATowerDefensePlayerController : public APlayerController
+{
+	GENERATED_UCLASS_BODY()
+
+	
+	
+};
